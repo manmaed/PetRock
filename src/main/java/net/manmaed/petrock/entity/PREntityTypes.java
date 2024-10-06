3fc7badfe38c2c01f2c8eb24bd3a287281e02d59
manmaed <manmaed18@gmail.com>
2024-10-06 13:59:47 +0100

    Ok we are going to do it the simple way lol

--- src/main/java/net/manmaed/petrock/entity/PREntityTypes.java
+++ src/main/java/net/manmaed/petrock/entity/PREntityTypes.java
@@ -2,25 +2,15 @@ package net.manmaed.petrock.entity;
 
 
 import net.manmaed.petrock.PetRock;
-import net.manmaed.petrock.libs.RLHelper;
-import net.minecraft.core.Holder;
-import net.minecraft.core.Registry;
-import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.core.registries.Registries;
-import net.minecraft.network.syncher.EntityDataSerializer;
-import net.minecraft.resources.ResourceKey;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.MobCategory;
-import net.minecraft.world.entity.animal.CatVariant;
 import net.neoforged.neoforge.registries.DeferredHolder;
 import net.neoforged.neoforge.registries.DeferredRegister;
 
 public class PREntityTypes {
 
     public static DeferredRegister<EntityType<?>> ENTITY_TYPES = DeferredRegister.create(Registries.ENTITY_TYPE, PetRock.MOD_ID);
-    public static final ResourceKey<Registry<RockVariant>> ROCK_VARIANT = ResourceKey.createRegistryKey(RLHelper.location("rock_variant"));
-    public static final Registry<RockVariant> REGISTRY_ROCK_VARIANT = BuiltInRegistries.registerSimple(ROCK_VARIANT, RockVariant::bootstrap);
-    public static final EntityDataSerializer<Holder<RockVariant>> EDS_ROCK_VARIANT = EntityDataSerializer.forValueType(RockVariant.STREAM_CODEC);
 
     public static final DeferredHolder<EntityType<?>, EntityType<EntityPetRock>> PETROCK = ENTITY_TYPES.register("petrock",
             () -> EntityType.Builder.of(EntityPetRock:: new, MobCategory.MISC)
