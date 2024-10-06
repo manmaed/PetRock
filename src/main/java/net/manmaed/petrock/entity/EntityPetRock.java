bb056e0361661c96f1183f0cf51c2dbda64f7e1e
manmaed <manmaed18@gmail.com>
2024-10-06 15:31:18 +0100

    New PetRocks

--- src/main/java/net/manmaed/petrock/entity/EntityPetRock.java
+++ src/main/java/net/manmaed/petrock/entity/EntityPetRock.java
@@ -2,17 +2,20 @@ package net.manmaed.petrock.entity;
 
 import net.manmaed.petrock.item.PRItems;
 import net.minecraft.core.BlockPos;
-import net.minecraft.core.Holder;
+import net.minecraft.nbt.CompoundTag;
 import net.minecraft.network.syncher.EntityDataAccessor;
+import net.minecraft.network.syncher.EntityDataSerializers;
 import net.minecraft.network.syncher.SynchedEntityData;
-import net.minecraft.resources.ResourceKey;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.sounds.SoundEvent;
 import net.minecraft.sounds.SoundEvents;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResult;
 import net.minecraft.world.damagesource.DamageSource;
-import net.minecraft.world.entity.*;
+import net.minecraft.world.entity.AgeableMob;
+import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.entity.Mob;
+import net.minecraft.world.entity.TamableAnimal;
 import net.minecraft.world.entity.ai.attributes.AttributeSupplier;
 import net.minecraft.world.entity.ai.attributes.Attributes;
 import net.minecraft.world.entity.ai.goal.*;
@@ -24,18 +27,22 @@ import net.minecraft.world.item.DyeItem;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.state.BlockState;
 
 import javax.annotation.Nullable;
 import java.util.UUID;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * Created by manmaed on 07/03/2021.
  */
-public class EntityPetRock extends TamableAnimal implements VariantHolder<Holder<RockVariant>> {
+public class EntityPetRock extends TamableAnimal {
 
-    private static final EntityDataAccessor<Holder<RockVariant>> DATA_VARIANT_ID = SynchedEntityData.defineId(EntityPetRock.class, PREntityTypes.EDS_ROCK_VARIANT);
-    private static final ResourceKey<RockVariant> DEFAULT_VARIANT = RockVariant.STONE;
+    private static final EntityDataAccessor<Integer> VARIANT_ID = SynchedEntityData.defineId(EntityPetRock.class, EntityDataSerializers.INT);
+
+
     protected EntityPetRock(EntityType<? extends TamableAnimal> type, Level worldIn) {
         super(type, worldIn);
         this.setTame(false, false);
@@ -53,6 +60,27 @@ public class EntityPetRock extends TamableAnimal implements VariantHolder<Holder
         this.targetSelector.addGoal(1, new OwnerHurtByTargetGoal(this));
         this.targetSelector.addGoal(2, new OwnerHurtTargetGoal(this));
         this.targetSelector.addGoal(3, (new HurtByTargetGoal(this)).setAlertOthers());
+    }
+
+    @Override
+    protected void defineSynchedData(SynchedEntityData.Builder synceddata) {
+        super.defineSynchedData(synceddata);
+        //synceddata.define(VARIANT_ID, random.nextInt(6)); //Randomly set the rocks variant
+        synceddata.define(VARIANT_ID, 0);
+    }
+
+    @Override
+    public void addAdditionalSaveData(CompoundTag compoundTag) {
+        super.addAdditionalSaveData(compoundTag);
+        compoundTag.putInt("rock_variant", this.getVariant());
+    }
+
+    @Override
+    public void readAdditionalSaveData(CompoundTag compoundTag) {
+        super.readAdditionalSaveData(compoundTag);
+        if (compoundTag.contains("rock_variant")) {
+            this.setVariant(compoundTag.getInt("rock_variant"));
+        }
     }
 
     public static AttributeSupplier.Builder createAttributes() {
@@ -98,6 +126,28 @@ public class EntityPetRock extends TamableAnimal implements VariantHolder<Holder
         }
     }
 
+    public boolean isRockVariantChangeItem(Item item) {
+        Item[] changeItems = {
+                //Add Items Here For allowed Variant Change
+                //will also need a texture and added to rightClickSetRockVariant & getVariantName
+                Blocks.STONE.asItem(),
+                Blocks.NETHERRACK.asItem(),
+                Blocks.END_STONE.asItem(),
+                Blocks.DEEPSLATE.asItem(),
+                Blocks.DIORITE.asItem(),
+                Blocks.GRANITE.asItem(),
+                Blocks.ANDESITE.asItem(),
+                Blocks.CLAY.asItem(),
+                Blocks.BEDROCK.asItem()
+        };
+        for (Item itemloop: changeItems) {
+            if (item == itemloop) {
+                return true;
+            }
+        }
+        return false;
+    }
+
     @Override
     public boolean isFood(ItemStack itemStack) {
         return itemStack == PRItems.STONEIUM.get().getDefaultInstance();
@@ -117,6 +167,10 @@ public class EntityPetRock extends TamableAnimal implements VariantHolder<Holder
                         itemStack.shrink(1);
                     }
                     this.heal(3.0F);
+                    return InteractionResult.SUCCESS;
+                }
+                if (isRockVariantChangeItem(item)) {
+                    rightClickSetRockVariant(itemStack, player);
                     return InteractionResult.SUCCESS;
                 }
                 if (!(item instanceof DyeItem)) {
@@ -165,13 +219,108 @@ public class EntityPetRock extends TamableAnimal implements VariantHolder<Holder
         return petRock;
     }
 
-    @Override
-    public void setVariant(Holder<RockVariant> variant) {
-        this.entityData.set(DATA_VARIANT_ID, variant);
+    public void setVariant(Integer variant) {
+        this.entityData.set(VARIANT_ID, variant);
+    }
+
+    public Integer getVariant() {
+        return this.entityData.get(VARIANT_ID);
+    }
+
+    public String getVariantName(){
+        int type = this.getVariant();
+        String name;
+        switch (type) {
+            case -1:
+                name = "missingno";
+                break;
+            case 1:
+                name = "nether";
+                break;
+            case 2:
+                name = "end";
+                break;
+            case 3:
+                name = "deepslate";
+                break;
+            case 4:
+                name = "diorite";
+                break;
+            case 5:
+                name = "granite";
+                break;
+            case 6:
+                name = "andesite";
+                break;
+            case 7:
+                name = "clay";
+                break;
+            case 8:
+                name = "bedrock";
+                break;
+            default:
+                name = "stone";
+                break;
+        }
+        return name;
+    }
+
+    private void rightClickSetRockVariant(ItemStack itemStack, Player player) {
+        Item item = itemStack.getItem();
+        String itemName = item.toString();
+        // Use regex to extract the part after the colon
+        String regex = "(?<=:)[^:]+";
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(itemName);
+        // Extract the part after the colon
+        String extractedName = "missingno"; //Should allways be replaced
+        if (matcher.find()) {
+            extractedName = matcher.group();
+        }
+        if (!player.getAbilities().instabuild) {
+            itemStack.shrink(1);
+        }
+        //System.out.println("Item We got " + extractedName);
+        switch (extractedName) {
+            case "stone":
+                setVariant(0);
+                break;
+            case "netherrack":
+                setVariant(1);
+                break;
+            case "end_stone":
+                setVariant(2);
+                break;
+            case "deepslate":
+                setVariant(3);
+                break;
+            case "diorite":
+                setVariant(4);
+                break;
+            case "granite":
+                setVariant(5);
+                break;
+            case "andesite":
+                setVariant(6);
+                break;
+            case "clay":
+                setVariant(7);
+                break;
+            case "bedrock":
+                setVariant(8);
+                break;
+            case "missingno":
+                setVariant(-1);
+                break;
+        }
     }
 
     @Override
-    public Holder<RockVariant> getVariant() {
-        return this.entityData.get(DATA_VARIANT_ID);
+    public void tick() {
+        super.tick();
+        //System.out.println(getVariant());
+        if (getVariant() == null) {
+            setVariant(0);
+        }
     }
 }
