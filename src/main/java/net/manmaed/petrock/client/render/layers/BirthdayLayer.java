d52d9d72d7f23a94dd68a81edaf1922edfafa232
manmaed <manmaed18@gmail.com>
2025-10-20 17:50:14 +0100

    Add HatData & SlowpokeMode onto the Petrock!

--- src/main/java/net/manmaed/petrock/client/render/layers/BirthdayLayer.java
+++ src/main/java/net/manmaed/petrock/client/render/layers/BirthdayLayer.java
@@ -2,8 +2,10 @@ package net.manmaed.petrock.client.render.layers;
 
 import com.mojang.blaze3d.vertex.PoseStack;
 import com.mojang.blaze3d.vertex.VertexConsumer;
+import net.manmaed.petrock.PetRock;
 import net.manmaed.petrock.client.model.PRModels;
 import net.manmaed.petrock.client.render.model.ModelCakeHat;
+import net.manmaed.petrock.entity.EntityPetRock;
 import net.manmaed.petrock.hats.PRHats;
 import net.manmaed.petrock.libs.RLHelper;
 import net.minecraft.client.model.geom.EntityModelSet;
@@ -31,7 +33,16 @@ public class BirthdayLayer extends RenderLayer {
 
     @Override
     public void render(PoseStack poseStack, MultiBufferSource multiBufferSource, int packedLight, Entity entity, float p_117353_, float p_117354_, float p_117355_, float p_117356_, float p_117357_, float p_117358_) {
-        if(PRHats.birthday) {
+        EntityPetRock entityPetRock = (EntityPetRock)entity;
+        String hatData = entityPetRock.getHatData();
+        if (hatData.equals("birthday")) {
+            poseStack.pushPose();
+            VertexConsumer vertexConsumer = multiBufferSource.getBuffer(RenderType.entityCutoutNoCull(skin));
+            poseStack.translate(0F, -0.5626F, 0F);
+            hat.renderToBuffer(poseStack, vertexConsumer, packedLight, OverlayTexture.NO_OVERLAY);
+            poseStack.popPose();
+        }
+        if (PRHats.birthday && hatData.isEmpty()) {
             poseStack.pushPose();
             VertexConsumer vertexConsumer = multiBufferSource.getBuffer(RenderType.entityCutoutNoCull(skin));
             poseStack.translate(0F, -0.5626F, 0F);
